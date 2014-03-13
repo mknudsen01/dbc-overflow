aca958c989717aad23c59cf2936204a8a1cebf39
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-13 16:16:50 -0700

    Require a value for the positive boolean

--- dbc_overflow/db/migrate/20140313230811_create_comments.rb
+++ dbc_overflow/db/migrate/20140313230811_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.boolean :positive
+      t.boolean :positive, null: false
       t.belongs_to :user
       t.references :voteable, polymorphic: true
     end
