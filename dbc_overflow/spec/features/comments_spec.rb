82c48331cfe4d486f793049a274bc76ef225ff5f
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-13 23:37:47 -0700

    Add pending feature tests for comments

---
+++ dbc_overflow/spec/features/comments_spec.rb
@@ -0,0 +1,24 @@
+describe 'Comments' do
+  describe 'adding a comment' do
+
+    describe 'question comment' do
+      context 'valid params' do
+        it 'adds a comment for a question'
+      end
+
+      context 'invalid params' do
+        it 'does not add a comment for a question'
+      end
+    end
+
+    describe 'answer comment' do
+      context 'valid params' do
+        it 'adds a comment for an answer'
+      end
+
+      context 'invalid params' do
+        it 'does not add a comment for an answer'
+      end
+    end
+  end
+end
