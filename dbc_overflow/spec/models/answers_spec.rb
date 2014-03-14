fbadc31bc78743e1605ed8067fbd4ebe331e494f
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-14 15:21:29 -0700

    uncommented out code that was commented out for testing

--- dbc_overflow/spec/models/answers_spec.rb
+++ dbc_overflow/spec/models/answers_spec.rb
@@ -1,10 +1,10 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe Answer do
-#   it { should validate_presence_of :body }
+describe Answer do
+  it { should validate_presence_of :body }
 
-#   context "associations" do
-#     it { should belong_to :question }
-#     it { should belong_to :user }
-#   end
-# end
+  context "associations" do
+    it { should belong_to :question }
+    it { should belong_to :user }
+  end
+end
