981aaa4a0a11c77308d84caeab28943d67d6fcd0
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-14 17:06:18 -0700

    adds passing session controller tests

--- dbc_overflow/spec/features/users_features_spec.rb
+++ dbc_overflow/spec/features/users_features_spec.rb
@@ -7,47 +7,52 @@ describe "Users", :js => true do
   context "homepage" do
     it "displays login form" do
       visit root_path
-      expect(page).to have_content category.name
+      expect(page).to have_button 'Login'
     end
-  end
 
-  context "category page" do
-    it "shows the name and the list of posts associated with it" do
+    it "displays login form" do
       visit root_path
-      click_on category.name
-      expect(page).to have_content category.name
-      expect(page).to have_content post.title
+      expect(page).to have_link 'Sign Up'
     end
   end
 
-  context "create a category" do
-    it "redirects to the category page if successful" do
-      visit new_category_path
-      fill_in "Name", :with => "business"
-      click_on "Create Category"
-      expect(page).to have_content "business"
-    end
+  # context "category page" do
+  #   it "shows the name and the list of posts associated with it" do
+  #     visit root_path
+  #     click_on category.name
+  #     expect(page).to have_content category.name
+  #     expect(page).to have_content post.title
+  #   end
+  # end
 
-    it "renders the form with errors if unsuccessful" do
-      visit new_category_path
-      click_on "Create Category"
-      expect(page).to have_content "Name can't be blank"
-    end
-  end
+  # context "create a category" do
+  #   it "redirects to the category page if successful" do
+  #     visit new_category_path
+  #     fill_in "Name", :with => "business"
+  #     click_on "Create Category"
+  #     expect(page).to have_content "business"
+  #   end
 
-  context "update a category" do
-    it "redirects to the category page if successful" do
-      visit edit_category_path(category)
-      fill_in "Name", :with => "business"
-      click_on "Update Category"
-      expect(page).to have_content "business"
-    end
+  #   it "renders the form with errors if unsuccessful" do
+  #     visit new_category_path
+  #     click_on "Create Category"
+  #     expect(page).to have_content "Name can't be blank"
+  #   end
+  # end
 
-    it "renders the form with errors if unsuccessful" do
-      visit edit_category_path(category)
-      fill_in "Name", :with => ""
-      click_on "Update Category"
-      expect(page).to have_content "Name can't be blank"
-    end
-  end
+  # context "update a category" do
+  #   it "redirects to the category page if successful" do
+  #     visit edit_category_path(category)
+  #     fill_in "Name", :with => "business"
+  #     click_on "Update Category"
+  #     expect(page).to have_content "business"
+  #   end
+
+  #   it "renders the form with errors if unsuccessful" do
+  #     visit edit_category_path(category)
+  #     fill_in "Name", :with => ""
+  #     click_on "Update Category"
+  #     expect(page).to have_content "Name can't be blank"
+  #   end
+  # end
 end
