41564a7680a182c312efbc19128cd03b55d386d2
Chermaine Zimmerman <chermainespence@Chermaines-MacBook-Pro.local>
2014-03-14 17:08:53 -0700

    uncomments out tests

--- dbc_overflow/spec/controllers/answers_controller_spec.rb
+++ dbc_overflow/spec/controllers/answers_controller_spec.rb
@@ -1,71 +1,71 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe AnswersController do
-#   let!(:question) { FactoryGirl.create :question }
-#   let!(:answer) { FactoryGirl.create :answer}
+describe AnswersController do
+  let!(:question) { FactoryGirl.create :question }
+  let!(:answer) { FactoryGirl.create :answer}
 
-#   context "#index" do
-#     it "is sucessful" do
-#       get :index, :question => question
-#       expect(response).to be_success
-#     end
+  context "#index" do
+    it "is sucessful" do
+      get :index, :question => question
+      expect(response).to be_success
+    end
 
-#     it "assigns @answers to Answer.all" do
-#       get :index
-#       expect(assigns(:answers)).to eq(Answer.all)
-#     end
-#   end
+    it "assigns @answers to Answer.all" do
+      get :index
+      expect(assigns(:answers)).to eq(Answer.all)
+    end
+  end
 
-#   context "#show" do
-#   end
+  context "#show" do
+  end
 
-#   context "#new" do
-#     it "is sucessful" do
-#       get :new, :question_id => question.id
-#       expect(response).to be_success
-#     end
-#   end
+  context "#new" do
+    it "is sucessful" do
+      get :new, :question_id => question.id
+      expect(response).to be_success
+    end
+  end
 
-#   context "#create" do
-#     it "with valid attributes" do
-#       expect {
-#         post :create, :question_id => question.id, :answer => attributes_for(:answer)
-#         }.to change { Answer.count }.by(1)
-#     end
+  context "#create" do
+    it "with valid attributes" do
+      expect {
+        post :create, :question_id => question.id, :answer => attributes_for(:answer)
+        }.to change { Answer.count }.by(1)
+    end
 
-#     it "with invalid attributes" do
-#       expect {
-#         post :create, :question_id => question.id, :answer => {}
-#         expect(response).to be_redirect
-#         }.to_not change { Answer.count }
-#     end
-#   end
+    it "with invalid attributes" do
+      expect {
+        post :create, :question_id => question.id, :answer => {}
+        expect(response).to be_redirect
+        }.to_not change { Answer.count }
+    end
+  end
 
-#   context "#edit" do
-#     it "is sucessful" do
-#       get :edit, :question_id => question.id, :id => answer.id
-#       expect(response).to be_success
-#     end
+  context "#edit" do
+    it "is sucessful" do
+      get :edit, :question_id => question.id, :id => answer.id
+      expect(response).to be_success
+    end
 
-#     it "assigns @answer to a particular answer" do
-#       get :edit, :question_id => question.id, :id => answer.id
-#       expect(assigns(:answer)).to eq(answer)
-#     end
-#   end
+    it "assigns @answer to a particular answer" do
+      get :edit, :question_id => question.id, :id => answer.id
+      expect(assigns(:answer)).to eq(answer)
+    end
+  end
 
-#   context "#update" do
-#     it "with valid attributes" do
-#       expect {
-#         put :update, :question_id => question.id, :id => answer.id, :answer => { :body => "CRAP!" }
-#         expect(response).to be_redirect
-#         }.to change { answer.reload.body }.from(answer.body).to("CRAP!")
-#     end
+  context "#update" do
+    it "with valid attributes" do
+      expect {
+        put :update, :question_id => question.id, :id => answer.id, :answer => { :body => "CRAP!" }
+        expect(response).to be_redirect
+        }.to change { answer.reload.body }.from(answer.body).to("CRAP!")
+    end
 
-#     it "with invalid attributes" do
-#       expect {
-#         put :update, :question_id => question.id, :id => answer.id, :answer => {}
-#         expect(response).to redirect_to(edit_question_answer_path)
-#         }.to_not change { answer.reload.body }
-#     end
-#   end
-# end
+    it "with invalid attributes" do
+      expect {
+        put :update, :question_id => question.id, :id => answer.id, :answer => {}
+        expect(response).to redirect_to(edit_question_answer_path)
+        }.to_not change { answer.reload.body }
+    end
+  end
+end
--- dbc_overflow/spec/features/users_features_spec.rb
+++ dbc_overflow/spec/features/users_features_spec.rb
@@ -16,43 +16,4 @@ describe "Users", :js => true do
     end
   end
 
-  # context "category page" do
-  #   it "shows the name and the list of posts associated with it" do
-  #     visit root_path
-  #     click_on category.name
-  #     expect(page).to have_content category.name
-  #     expect(page).to have_content post.title
-  #   end
-  # end
-
-  # context "create a category" do
-  #   it "redirects to the category page if successful" do
-  #     visit new_category_path
-  #     fill_in "Name", :with => "business"
-  #     click_on "Create Category"
-  #     expect(page).to have_content "business"
-  #   end
-
-  #   it "renders the form with errors if unsuccessful" do
-  #     visit new_category_path
-  #     click_on "Create Category"
-  #     expect(page).to have_content "Name can't be blank"
-  #   end
-  # end
-
-  # context "update a category" do
-  #   it "redirects to the category page if successful" do
-  #     visit edit_category_path(category)
-  #     fill_in "Name", :with => "business"
-  #     click_on "Update Category"
-  #     expect(page).to have_content "business"
-  #   end
-
-  #   it "renders the form with errors if unsuccessful" do
-  #     visit edit_category_path(category)
-  #     fill_in "Name", :with => ""
-  #     click_on "Update Category"
-  #     expect(page).to have_content "Name can't be blank"
-  #   end
-  # end
 end
