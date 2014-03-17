398a3ab6946ee81ecebe750b0a4b4ca6761dbf1f
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-16 18:29:55 -0700

    Add the Ajax functionnality for votes. * Clicking launches an ajax request, that is caught on the other hand and that replaces the votecount right next.

--- dbc_overflow/app/assets/javascripts/application.js
+++ dbc_overflow/app/assets/javascripts/application.js
@@ -13,3 +13,57 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+DBCOverFlow = {};
+
+DBCOverFlow.Controller = function(opts) {
+    this.view = opts.view;
+};
+DBCOverFlow.Controller.prototype = {
+    votedAction: function(e, response) {
+        var votecount = response.responseJSON.votecount;
+        var target = e.target
+        this.view.showVotes(target, votecount);
+    }
+};
+
+DBCOverFlow.View = function() {}
+DBCOverFlow.View.prototype = {
+    showVotes: function(target, votecount) {
+        $(target).parents('.votes').find('.vote-count').text(votecount);
+    }
+}
+
+
+DBCOverFlow.Binder = function(opts) {
+    this.controller = opts.controller;
+};
+DBCOverFlow.Binder.prototype = {
+    bind: function() {
+        this.bindUpVote();
+        this.bindDownVote();
+    },
+    bindUpVote: function() {
+        this.bindVoted('.upvote');
+    },
+    bindDownVote: function() {
+        this.bindVoted('.downvote');
+    },
+    bindVoted: function(vote) {
+        var self = this;
+        $("body").on("ajax:complete", vote, function(e, response) {
+            console.log('click')
+            self.controller.votedAction(e, response);
+        });
+    }
+};
+
+$(document).ready(function() {
+    DBCOverFlow.view = new DBCOverFlow.View();
+    DBCOverFlow.controller = new DBCOverFlow.Controller({
+        view: DBCOverFlow.view
+    });
+    DBCOverFlow.binder = new DBCOverFlow.Binder({
+        controller: DBCOverFlow.controller
+    }).bind()
+});
